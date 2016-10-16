6af74dcb3fbf7332301e809aafc317125714209f
Paul Fasola <paul.fasola@hotmail.fr>
2016-10-17 01:38:40 +0200

    Update BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,8 +9,7 @@ include vendor/huawei/next/BoardConfigVendor.mk
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-TARGET_BOOTLOADER_BOARD_NAME := NXT
+TARGET_BOOTLOADER_BOARD_NAME := hi3650
 
 # Architecture
 TARGET_ARCH := arm64
@@ -26,9 +25,6 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a7
 
 TARGET_CPU_SMP := true
-TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a15 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mtune=cortex-a15 -mfloat-abi=softfp
-TARGET_EXTRA_CFLAGS := -mtune=cortex-a15 -mcpu=cortex-a15
 TARGET_BOARD_PLATFORM := hi3650
 TARGET_SOC := kirin950
 
@@ -44,20 +40,37 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE  := mmcparts=mmcblk0:p1(vrl),p2(vrl_backup),p6(modemnvm_factory),p9(splash),p10(modemnvm_backup),p11(modemnvm_img),p12(modemnvm_system),p14(3rdmodemnvm),p15(3rdmodemnvmbkp),p18(modem_om),p21(modemnvm_update),p31(modem),p32(modem_dsp),p35(3rdmodem) loglevel=4 androidboot.selinux=permissive
+
 BOARD_KERNEL_BASE     := 0x00478000
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x07b88000 --tags_offset 0x07588000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_SOURCE := kernel/huawei/malit880
-TARGET_KERNEL_CONFIG := merge_hi3650_defconfig
+BOARD_KERNEL_TAGS_OFFSET := 0x07588000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x07b88000
+
+BOARD_KERNEL_CMDLINE  := mmcparts=mmcblk0:p1(vrl),p2(vrl_backup),p6(modemnvm_factory),p9(splash),p10(modemnvm_backup),p11(modemnvm_img),p12(modemnvm_system),p14(3rdmodemnvm),p15(3rdmodemnvmbkp),p18(modem_om),p21(modemnvm_update),p31(modem),p32(modem_dsp),p35(3rdmodem) loglevel=4 androidboot.selinux=permissive
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+TARGET_PREBUILT_KERNEL := device/huawei/next-kernel/kernel
+
+#TARGET_KERNEL_SOURCE := kernel/huawei/malit880
+#TARGET_KERNEL_CONFIG := merge_hi3650_defconfig
+#TARGET_KERNEL_CUSTOM_TOOLCHAIN := aarch64-linux-android-4.9
+#TARGET_KERNEL_CROSS_COMPILE_PREFIX := /home/paul/Desktop/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 # Partitions
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+#BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 268435456
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 10737418240
+#BOARD_USERDATAIMAGE_PARTITION_SIZE := 107743281152
+
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 26935820288
-BOARD_FLASH_BLOCK_SIZE := 131072
-TARGET_USERIMAGES_USE_EXT4 := true
+
+BOARD_FLASH_BLOCK_SIZE := 4096
 
 # USB mass storage
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
@@ -77,6 +90,15 @@ ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mtp,adb
 # Recovery
 RECOVERY_FSTAB_VERSION := 2
 TARGET_RECOVERY_FSTAB := device/huawei/next/recovery/recovery.fstab
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
 
 # SELinux
 #BOARD_SEPOLICY_DIRS += \
