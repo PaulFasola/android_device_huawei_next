82e706616cce77b16e85dfa1ddc02d673c837c97
Paul Fasola <paul.fasola@hotmail.fr>
2016-10-17 01:43:42 +0200

    Deep config files update

---
+++ rootdir/Android.mk
@@ -0,0 +1,466 @@
+LOCAL_PATH:= $(call my-dir)
+
+
+#######################################
+# init.balong_modem.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.balong_modem.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.61276.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.61276.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.61259.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.61259.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.chip.usb.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.chip.usb.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.62010.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.62010.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.3741.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.3741.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.3583.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.3583.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.recovery.hisi.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.hisi.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.connectivity.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.connectivity.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.connectivity.bcm43455.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.connectivity.bcm43455.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# fstab.hi3650
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.hi3650
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.3583.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.3583.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.hi3650.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.hi3650.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.connectivity.bcm43xx.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.connectivity.bcm43xx.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.3584.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.3584.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.3584.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.3584.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.audio.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.audio.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.zygote64_32.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.zygote64_32.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.41038.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.41038.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.62010.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.62010.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.protocol.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.protocol.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.post-fs-data.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.post-fs-data.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.extmodem.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.extmodem.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.61297.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.61297.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.performance.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.performance.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.71315.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.71315.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.tee.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.tee.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.hi3650.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.hi3650.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.connectivity.hisi.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.connectivity.hisi.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.61262.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.61262.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.3741.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.3741.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.manufacture.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.manufacture.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.71315.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.71315.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.device.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.device.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.zygote32.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.zygote32.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.platform.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.platform.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.connectivity.gps.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.connectivity.gps.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.61262.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.61262.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.hisi.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.hisi.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.61297.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.61297.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.trace.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.trace.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.61259.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.61259.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.61276.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.61276.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# ueventd.41038.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.41038.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.recovery.huawei.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.huawei.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+#######################################
+# init.usb.rc
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.usb.rc
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+
+
+# Total 50 files added on 2016-10-15T17:05:41.947805 UTC
