780b7aa83a51dddacab78e085f946059ca45d9ed
Paul Fasola <paul.fasola@hotmail.fr>
2016-12-03 20:24:28 +0100

    Enable dex-preop for userdebug
    Change-Id: Ic58dab6d93ff09930209c14166e4442dac0dee40

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,7 +121,7 @@ TW_MAX_BRIGHTNESS := 255
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
+  ifneq ($(TARGET_BUILD_VARIANT),userdebug)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
